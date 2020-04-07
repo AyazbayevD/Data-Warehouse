8ad7efd6cc39b5abc407019422a66ac694600de2
iskander <iskabaur@gmail.com>
2020-04-07 15:48:08 +0300

    server API

--- API/server.py
+++ API/server.py
@@ -1,23 +1,36 @@
-import socket
+import os
 
-HOST = ''
-PORT = ''
+from flask import Flask, request, abort, jsonify, send_from_directory
+
+api = Flask(__name__)
+UPLOAD_DIRECTORY = "/home/iskander/PycharmProjects/Data-Warehouse/temp_files"
 
 
-def recevive_files():
-    s = socket.socket()
-    s.bind((HOST, PORT))
-    s.listen(5)
-    print("Server listening...")
-    cnt = 0
-    while True:
-        conn, addr = s.accept()
-        print('Got connection from', addr)
-        data = conn.recv(1024)
-        print('data=%s', (data))
-        if not data:
-            break
-        format = str(data).split('.')[1]
-        filename = f'temp_files/file_{cnt}.{format}'
-        file = open(filename, 'w')
-        file.write(data)
+@api.route("/files/<path>")
+def get_file(path):
+    """Download a file."""
+    return send_from_directory(UPLOAD_DIRECTORY, path, as_attachment=True)
+
+
+@api.route("/files/<filename>", methods=["POST"])
+def post_file(filename):
+    """Upload a file."""
+
+    if "/" in filename:
+        # Return 400 BAD REQUEST
+        abort(400, "no subdirectories directories allowed")
+
+    with open(os.path.join(UPLOAD_DIRECTORY, filename), "wb") as fp:
+        fp.write(request.data)
+
+    # Return 201 CREATED
+    return "", 201
+
+
+@api.route('/')
+def hello():
+    print('Hello')
+
+
+if __name__ == "__main__":
+    api.run(debug=True, port=2036)
